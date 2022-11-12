0d77bdc7b0702c551dbf9d80cc4ede242438712a
unknown <abood.elfar@yahoo.com>
2022-11-12 21:29:33 +0200

    init

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+func main() {
+	// var card string = "hi"
+	// card = "12" + "12"
+	// card := newCard()
+
+	// cards := []string{
+	// 	"hi",
+	// 	"hdsadsa",
+	// 	"byr"}
+
+	cards := deck{"test"}
+
+	cards = append(cards, "dsadsa")
+
+	// fmt.Println(cards)
+
+	// fmt.Println(card)
+	// fmt.Println(cards2)
+
+	cards.print()
+
+	//cards := newDeck()
+	//cards.print()
+}
+
+// func newCard() string {
+// 	return "hhhhhhhhhh"
+// }
